700fab96a5ab6060a8a384ff807ffc91e943eb78
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-10-02 16:39:01 +0530

    Fixed an issue where text color was not changing in most cases

--- scripts/main.js
+++ scripts/main.js
@@ -6,16 +6,16 @@ function randombg(){
     var a = randomNumber(0, 255);
     var b = randomNumber(0, 255);
     var c = randomNumber(0, 255);
-    var halfsum= (a+b+c)/2;
+    var sum= a+b+c;
     var background="rgb("+ a + ", " + b + ", " + c + ")";
 
-    if(halfsum>=325){
+    if(sum>=382.5){
         var bglist = document.querySelectorAll(".rand_back");
         var i;
         for (i = 0; i < bglist.length; i++) {
             bglist[i].style.color = ("#1e1e1e");
         }
-            document.getElementById("button").style.boxShadow=("5px 5px 15px #acacacaa");
+        document.getElementById("button").style.boxShadow=("5px 5px 15px #acacacaa");
     }
     else{
         var bglist = document.querySelectorAll(".rand_back");
